20c09fea39d22ab70e7e940b3c11faa4e4f286e2
Md Nouman <noumantannu06@gmail.com>
2026-05-02 23:13:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+ const toggles = document.querySelectorAll(".toggle");
+
+    toggles.forEach((toggle) => {
+      toggle.addEventListener("change", function () {
+        const checkedToggles = [...toggles].filter((t) => t.checked);
+
+        if (checkedToggles.length > 2) {
+          if (this.id === "good") {
+            document.getElementById("cheap").checked = false;
+          } else if (this.id === "cheap") {
+            document.getElementById("fast").checked = false;
+          } else if (this.id === "fast") {
+            document.getElementById("good").checked = false;
+          }
+        }
+      });
+    });
